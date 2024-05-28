c9f55e07c340b0f25b0bfd30a594ebe95cd86c53
Hector Matus <{ID}+{username}@users.noreply.github.com>
2024-05-28 17:13:25 -0600

    add save token route when login

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,4 +1,15 @@
 // * Routes must be named using route.ts and add dirs according of the name of the route
-export async function POST(req: Request) {
-  return Response.json({ message: 'Login successful'});
+import jwt from "jsonwebtoken"
+import { cookies } from 'next/headers'
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request, res: Response) {
+  const { email, password } = await req.json();
+  // Hardcoded login admin
+  if (email === 'admin@gmail.com' && password === 'admin') {
+    const token = jwt.sign({email, password, expiration: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30}, 'secret')
+    cookies().set('jwt', token, {secure: process.env.NODE_ENV === 'production', httpOnly: true, sameSite: 'strict'})
+    return new Response(JSON.stringify({message: 'Logged in'}), {status: 200, headers: {'Content-Type': 'application/json'}})
+  }
+  return new Response(JSON.stringify({message: 'Invalid credentials'}), {status: 401, headers: {'Content-Type': 'application/json'}})
 }
